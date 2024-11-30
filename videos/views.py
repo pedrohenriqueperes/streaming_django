c343aaa21c1828d6a97e7d1c49d8ff3182142bf5
pedrohenriqueperes <php741@protonmail.com>
2024-11-29 21:26:39 -0300

    paginator

--- videos/views.py
+++ videos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.conf import settings
+from django.core.paginator import Paginator  # Adicione esta linha
 import os
 
 def get_videos():
@@ -16,13 +17,26 @@ def get_videos():
     return sorted(videos, key=lambda x: x['name'])
 
 def video_list(request):
-    """Lista todos os vídeos da pasta"""
+    """Lista todos os vídeos da pasta com paginação"""
+    # Pega a lista completa de vídeos
     videos = get_videos()
-    return render(request, 'videos/video_list.html', {'videos': videos})
+    
+    # Cria um objeto de paginação com 9 itens por página
+    paginator = Paginator(videos, 9)
+    
+    # Pega o número da página da URL (?page=1)
+    page_number = request.GET.get('page', 1)
+    
+    # Obtém os objetos da página atual
+    page_obj = paginator.get_page(page_number)
+    
+    return render(request, 'videos/video_list.html', {
+        'videos': page_obj.object_list,  # Lista de vídeos da página atual
+        'page_obj': page_obj  # Objeto de paginação completo
+    })
 
 def video_detail(request, video_name):
     """Mostra um vídeo específico"""
     videos = get_videos()
     video = next((v for v in videos if v['file'] == video_name), None)
     return render(request, 'videos/video_detail.html', {'video': video})
-# Create your views here.
